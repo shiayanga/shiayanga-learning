4255b741c0e83004852612a316d9f40ce5063d21
和信金谷-李洋 <1499271883@qq.com>
2023-12-06 17:55:03 +0800

    测试配置文件

---
+++ springboot01_demo/src/main/java/com/github/shiayanga/Springboot01DemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.shiayanga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot01DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot01DemoApplication.class, args);
+    }
+
+}
---
+++ springboot01_demo/src/main/java/com/github/shiayanga/pojo/Pet.java
@@ -0,0 +1,2 @@
+package com.github.shiayanga.pojo;public class Pet {
+}
