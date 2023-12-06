a51f57916bcc42b7376f1db263c045ac3487f92c
和信金谷-李洋 <1499271883@qq.com>
2023-12-06 21:47:45 +0800

    测试加载配置文件

--- springboot01_demo/src/test/java/com/github/shiayanga/springboot01_demo/Springboot01DemoApplicationTests.java
+++ springboot01_demo/src/test/java/com/github/shiayanga/springboot01_demo/Springboot01DemoApplicationTests.java
@@ -1,13 +1,18 @@
 package com.github.shiayanga.springboot01_demo;
 
+import com.github.shiayanga.pojo.Person;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Springboot01DemoApplicationTests {
 
+    @Resource
+    Person person;
     @Test
     void contextLoads() {
+        System.out.println(person);
     }
 
 }
