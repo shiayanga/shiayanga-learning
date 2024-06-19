88dc2832d8510a75296bdb9e559edd2dac267d27
Shiayanga <1499271883@qq.com>
2024-06-19 17:49:44 +0800

    权重随机算法

--- algorithm/src/main/java/com/github/algorithm/random/WeightedRandom.java
+++ algorithm/src/main/java/com/github/algorithm/random/WeightedRandom.java
@@ -19,13 +19,13 @@ public class WeightedRandom {
         weightedRandomPojos.add(weightedRandomPojo2);
         weightedRandomPojos.add(weightedRandomPojo3);
         weightedRandomPojos.add(weightedRandomPojo4);
-        WeightedRandom weightedRandom = new WeightedRandom();
-        if (weightedRandom.weightMap.isEmpty()) {
-            weightedRandom.setMap(weightedRandomPojos);
-        }
+//        WeightedRandom weightedRandom = new WeightedRandom();
+//        if (weightedRandom.weightMap.isEmpty()) {
+//            weightedRandom.setMap(weightedRandomPojos);
+//        }
         Map<String, Integer> count = new HashMap<>();
         for (int i = 0; i < 10000; i++) {
-            String text = weightedRandom.getText();
+            String text = getText(weightedRandomPojos);
             if (count.get(text) != null) {
                 Integer num = count.get(text);
                 count.put(text, num + 1);
@@ -37,8 +37,8 @@ public class WeightedRandom {
         for (String s : count.keySet()) {
             Integer nums = count.get(s);
             BigDecimal bigDecimal = new BigDecimal(nums);
-            BigDecimal decimal = bigDecimal.divide(totalTimes, 2, RoundingMode.HALF_UP);
-            System.out.println(s + "\t命中\t" + count.get(s) + "次\t占比\t" + decimal.doubleValue()*100 + "%");
+            BigDecimal decimal = bigDecimal.divide(totalTimes, 20, RoundingMode.HALF_UP);
+            System.out.println(s + "\t命中\t" + count.get(s) + "次\t占比\t" + decimal.doubleValue() * 100 + "%");
         }
     }
 
@@ -55,6 +55,18 @@ public class WeightedRandom {
             System.out.println("总权重：" + totalWeight);
         }
         return true;
+    }
+
+    public static String getText(List<WeightedRandomPojo> weightList) {
+        TreeMap<Double, String> weightMap1 = new TreeMap<>();
+        for (WeightedRandomPojo weightedRandomPojo : weightList) {
+            Double weight = weightedRandomPojo.getWeight();
+            Double lastWeight = weightMap1.isEmpty() ? 0 : weightMap1.lastKey();
+            weightMap1.put(weight + lastWeight, weightedRandomPojo.getText());
+        }
+        double random = weightMap1.lastKey() * Math.random();
+        NavigableMap<Double, String> tailedMap = weightMap1.tailMap(random, false);
+        return weightMap1.get(tailedMap.firstKey());
     }
 
     public String getText() {
